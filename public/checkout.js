3eb4bfc523914e10293f4ff5e64596c14fd75016
Valeria UEHARA <41277225+vuehara@users.noreply.github.com>
2022-07-18 11:55:53 -0300

    Update checkout.js

--- public/checkout.js
+++ public/checkout.js
@@ -41,7 +41,7 @@ async function handleSubmit(e) {
     elements,
     confirmParams: {
       // Make sure to change this to your payment completion page
-      return_url: "http://localhost:4242/checkout.html",
+      return_url: "https://html-node-stripe.herokuapp.com/",
     },
   });
 
